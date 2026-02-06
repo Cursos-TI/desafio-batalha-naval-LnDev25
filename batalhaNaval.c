65039a0091ca334b8ad3e9ddd8cd3e310ea5aeaa
Nathan <suporteforlucas@hotmail.com>
2026-02-06 19:31:18 +0000

    Finalizando Batalha Naval - Nível Novato (Matrizes)

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,40 +1,60 @@
 #include <stdio.h>
 
-// Desafio Batalha Naval - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
-// Siga os comentários para implementar cada parte do desafio.
-
 int main() {
-    // Nível Novato - Posicionamento dos Navios
-    // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
-    // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
-    // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
 
-    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
-    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
-    // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
-    // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
 
-    // Nível Mestre - Habilidades Especiais com Matrizes
-    // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
-    // Sugestão: Utilize estruturas de repetição aninhadas para preencher as áreas afetadas por essas habilidades no tabuleiro.
-    // Sugestão: Exiba o tabuleiro com as áreas afetadas, utilizando 0 para áreas não afetadas e 1 para áreas atingidas.
+   // INICIO DO PROGRAMA - NÍVEL NOVATO - TEMA 5
 
-    // Exemplos de exibição das habilidades:
-    // Exemplo para habilidade em cone:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 1 1 1 1 1
-    
-    // Exemplo para habilidade em octaedro:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 0 0 1 0 0
 
-    // Exemplo para habilidade em cruz:
-    // 0 0 1 0 0
-    // 1 1 1 1 1
-    // 0 0 1 0 0
+   // Tamanho do Tabuleiro 
+   int tabuleiro [10] [10];
 
-    return 0;
+   for (int agua = 0; agua < 10; agua ++) {
+
+    for (int agua2 = 0; agua2 < 10; agua2 ++) {
+
+        tabuleiro [agua] [agua2] = 0 ;
+    }
+
+   }
+
+
+   // Tamanho dos navios
+   int navio_horizontal [3] = {3, 3, 3} ;
+   int navio_vertical [3] = {3, 3, 3} ;
+
+
+   // Posicionando o Navio Horizontal
+   for (int posicao_horizontal = 0; posicao_horizontal < 3; posicao_horizontal ++) {
+
+    tabuleiro [2] [2 + posicao_horizontal] = navio_horizontal[posicao_horizontal];
+
+   }
+
+
+   // Posicionando o Navio Vertical
+   for (int posicao_vertical = 0; posicao_vertical < 3; posicao_vertical ++) {
+
+    tabuleiro [5 + posicao_vertical] [5] = navio_vertical [posicao_vertical];
+
+   }
+
+
+   // Exibindo o Tabuleiro Completo
+   printf("\n--- Batalha Naval ---\n");
+
+   for (int linha = 0; linha < 10; linha++) {
+        for (int coluna = 0; coluna < 10; coluna++) {
+
+        printf("%d ", tabuleiro[linha][coluna]);
+    }
+   
+    printf("\n"); 
+}
+
+
+   // FIM DO PROGRAMA - NÍVEL NOVATO - TEMA 5
+
+   
+   return 0;
 }
