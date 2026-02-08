bd56f43eb206409b3b609ee29293e2edeaa76785
Nathan <suporteforlucas@hotmail.com>
2026-02-08 22:13:30 +0000

    Finalizando nível aventureiro: Navios diagonais adicionados

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
 
+
+// INICIO DO PROGRAMA - NÍVEL NOVATO - TEMA 5
+
+
+// Definindo uma constante para o tamanho do tabuleiro
+#define TAM_TABULEIRO 10
+
 int main() {
 
+   int tabuleiro [TAM_TABULEIRO] [TAM_TABULEIRO];
 
-   // INICIO DO PROGRAMA - NÍVEL NOVATO - TEMA 5
+   for (int agua = 0; agua < TAM_TABULEIRO; agua ++) {
 
-
-   // Tamanho do Tabuleiro 
-   int tabuleiro [10] [10];
-
-   for (int agua = 0; agua < 10; agua ++) {
-
-    for (int agua2 = 0; agua2 < 10; agua2 ++) {
+    for (int agua2 = 0; agua2 < TAM_TABULEIRO; agua2 ++) {
 
         tabuleiro [agua] [agua2] = 0 ;
     }
@@ -19,15 +21,34 @@ int main() {
    }
 
 
-   // Tamanho dos navios
-   int navio_horizontal [3] = {3, 3, 3} ;
-   int navio_vertical [3] = {3, 3, 3} ;
+
+   // INICIO DO PROGRAMA - NÍVEL AVENTUREIRO - TEMA 5
+
+
+   // Posicionando o Navio Diagonal 1
+   for (int posicao_diagonal1 = 0; posicao_diagonal1 < 3; posicao_diagonal1 ++) {
+
+    tabuleiro [1 + posicao_diagonal1] [1 + posicao_diagonal1] = 3;
+
+   }
+
+   
+   // Posicionando o Navio Diagonal 2
+   for (int posicao_diagonal2 = 0; posicao_diagonal2 < 3; posicao_diagonal2 ++) {
+
+    tabuleiro [7 + posicao_diagonal2] [8 - posicao_diagonal2] = 3;
+    
+   }
+
+   
+   // FIM DO PROGRAMA - NÍVEL AVENTUREIRO - TEMA 5
+   
 
 
    // Posicionando o Navio Horizontal
    for (int posicao_horizontal = 0; posicao_horizontal < 3; posicao_horizontal ++) {
 
-    tabuleiro [2] [2 + posicao_horizontal] = navio_horizontal[posicao_horizontal];
+    tabuleiro [0] [2 + posicao_horizontal] = 3 ;
 
    }
 
@@ -35,26 +56,30 @@ int main() {
    // Posicionando o Navio Vertical
    for (int posicao_vertical = 0; posicao_vertical < 3; posicao_vertical ++) {
 
-    tabuleiro [5 + posicao_vertical] [5] = navio_vertical [posicao_vertical];
+    tabuleiro [5 + posicao_vertical] [5] = 3 ;
 
    }
 
 
-   // Exibindo o Tabuleiro Completo
-   printf("\n--- Batalha Naval ---\n");
+    // --- EXIBIÇÃO DO TABULEIRO ---
+    printf("\n--- Batalha Naval (Nível Aventureiro) ---\n\n");
 
-   for (int linha = 0; linha < 10; linha++) {
-        for (int coluna = 0; coluna < 10; coluna++) {
+    for (int linha = 0; linha < TAM_TABULEIRO; linha++) {
 
-        printf("%d ", tabuleiro[linha][coluna]);
+        for (int coluna = 0; coluna < TAM_TABULEIRO; coluna++) {
+
+            printf("%d ", tabuleiro[linha][coluna]);
+
+        }
+
+        printf("\n"); 
+
     }
-   
-    printf("\n"); 
-}
 
 
    // FIM DO PROGRAMA - NÍVEL NOVATO - TEMA 5
 
    
    return 0;
+
 }
